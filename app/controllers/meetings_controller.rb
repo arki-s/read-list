58f7a47813383832f91f1bde92309ec8b7d90fe9
exec zsh <wind10cloud@gmail.com>
2023-07-02 10:38:33 +0900

    added controllers

---
+++ app/controllers/meetings_controller.rb
@@ -0,0 +1,32 @@
+class MeetingsController < ApplicationController
+  def index
+    start_date = params.fetch(:start_date, Date.today).to_date
+
+    @meetings = Meeting.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def meeting_params
+  end
+
+end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,14 @@
 class Meeting < ApplicationRecord
   belongs_to :book
   has_one :record
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates :start_date, :end_date, presence: true
+  default_scope -> { order(:start_time) }
+
+  def time
+    "#{start_time.strftime('%I:%M %p')} - #{end_time.strftime('%I:%M %p')}"
+  end
+
+  def multi_days?
+    (end_time.to_date - start_time.to_date).to_i >= 1
+  end
 end
