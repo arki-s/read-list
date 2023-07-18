c9b3d3f8a410ca617fe7c4975719f62bc777af48
exec zsh <wind10cloud@gmail.com>
2023-07-18 12:44:53 +0900

    in progress

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,5 +10,5 @@ application.register("flatpickr", FlatpickrController)
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import Chart from 'stimulus-chartjs'
-application.register('chart', Chart)
+import InsertInListController from "./insert_in_list_controller"
+application.register("insert-in-list", InsertInListController)
